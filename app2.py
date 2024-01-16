1c87141114b873b3325fa5ab3e80a1633523aee0
aakash4210 <aakashsonawane11@gmail.com>
2024-01-16 11:29:18 +0530

    commit

--- app2.py
+++ app2.py
@@ -1,4 +1,6 @@
 
+# 98% accuracy
+
 from flask import Flask,request,jsonify,render_template
 import numpy as np
 import pandas as pd
@@ -24,7 +26,7 @@ def index():
 @app.route("/saleryPredic",methods=['GET','POST'])
 def predicpackage():
     if request.method == "POST":
-        #taked input form web (take name = of the field)
+        #taked input form web
         yearE=float(request.form.get('yearE'))
         fname=str(request.form.get('fname'))
 
@@ -38,14 +40,14 @@ def predicpackage():
         print("trim:-",result)
         
         #return render_template('sal.html',fname=fname,result=result[0])
-        return render_template('sal.html',fname=fname,result=result)
+        return render_template('c1.html',fname=fname,result=result)
     
     else:
-        return render_template('sal.html')
+        return render_template('c1.html')
         
 
 
-
+#https://gray-teacher-xgqts.pwskills.app:5000/saleryPredic
 
 if __name__=="__main__":
     app.run(host="0.0.0.0")
